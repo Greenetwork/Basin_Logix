db8130b60c2256f5a4cf39457d43506b235e48c0
Abdel <adiaa.kc@gmail.com>
2020-05-03 13:17:08 -0700

    updated to conduit

--- data_logix/conduit.py
+++ data_logix/conduit.py
@@ -22,14 +22,13 @@ def check_for_conduit_input_data():
     current_dir = os.getcwd()
     if "Basin_Logix" in current_dir.split("\\"):
             logging.warning("Basin_Logix directory detected - all input files will be referenced from this directory")
-        else:
-            logging.warning(" PLEASE CHANGE YOUR DIRECTORY TO BASIN LOGIX REPO - "
+    else:
+        logging.warning(" PLEASE CHANGE YOUR DIRECTORY TO BASIN LOGIX REPO - "
                             " for input files to work your current dir needs to be ~Basin_Logix") #showing user current dir
 
 def check_for_conduit_input_files():
     current_dir = os.getcwd()
-    conduit_input_file_path = "\data_logix\conduit_input_data"
-    full_path = os.path.join(current_dir + conduit_input_file_path)
+    full_path = os.path.join(current_dir)
     logging.warning(f"checking for input files in {full_path}")
     list_of_files_in_data_path = os.listdir(full_path)
     if "WATER_DISTRICTS.geojson" in list_of_files_in_data_path:
@@ -44,31 +43,31 @@ def check_for_conduit_input_files():
         logging.warning("MISSING INPUT FILES")
 
 def load_water_districts_data():
-    water_districts = gpd.read_file("data_logix/conduit_input_data/WATER_DISTRICTS.geojson")
+    water_districts = gpd.read_file("WATER_DISTRICTS.geojson")
     assert water_districts.crs == {'init': 'epsg:4326'}
     assert type(water_districts.geometry) == (gpd.geoseries.GeoSeries)
-    logging.warning("water districts data loaded")
+    logging.warning("WATER DISTRICTS data - Loaded")
     return water_districts
 
 def load_california_parcel_data():
-    apn_parcels = gpd.read_file("data_logix/conduit_input_data/PARCELS.geojson")
+    apn_parcels = gpd.read_file("PARCELS.geojson")
     assert apn_parcels.crs == {'init': 'epsg:4326'} # making sure input data file is in the right crs
     assert type(apn_parcels.geometry) == (gpd.geoseries.GeoSeries) #making sure i have a geom as geoseries
-    logging.warning("parcel data loaded")
+    logging.warning("PARCELS data - Loaded")
     return apn_parcels
 
 def load_california_crops_data():
-    crops = gpd.read_file("data_logix/conduit_input_data/CROPS.geojson")
+    crops = gpd.read_file("CROPS.geojson")
     assert crops.crs == {'init': 'epsg:4326'} # making sure input data file is in the right crs
     assert type(crops.geometry) == (gpd.geoseries.GeoSeries) #making sure i have a geom as geoseries
-    logging.warning("crop data loaded")
+    logging.warning("CROP data - Loaded")
     return crops
 
 def load_california_counties_data():
-    counties = gpd.read_file("data_logix/conduit_input_data/COUNTIES.geojson")
+    counties = gpd.read_file("COUNTIES.geojson")
     assert counties.crs == {'init': 'epsg:4326'} # making sure input data file is in the right crs
     assert type(counties.geometry) == (gpd.geoseries.GeoSeries) #making sure i have a geom as geoseries
-    logging.warning("counties data loaded")
+    logging.warning("COUNTIES data - Loaded")
     return counties
 
 def get_geom_from_county_name(counties_data_frame, selected_county:str):
@@ -76,16 +75,10 @@ def get_geom_from_county_name(counties_data_frame, selected_county:str):
     return selected_county_geom
 
 def filter_data_by_county(data_frame_to_filter, county_geom):
-crop_information_san_joaquin = gpd.sjoin(data_frame_to_filter, county_geom, op='within')  # lets add
-data_frame_to_filter = data_frame_to_filter.drop(columns=['STATEFP', 'COUNTYFP', 'COUNTYNS',
-                                                                                'GEOID', 'NAME', 'NAMELSAD', 'LSAD',
-                                                                                'CLASSFP', 'MTFCC', 'CSAFP', 'CBSAFP',
-                                                                                'METDIVFP', 'FUNCSTAT'])
-
-dwr_water_districts_san_joaquin = dwr_water_districts_san_joaquin.drop(columns=['STATEFP', 'COUNTYFP', 'COUNTYNS',
-                                                                                'GEOID', 'NAME', 'NAMELSAD', 'LSAD',
-                                                                                'CLASSFP', 'MTFCC', 'CSAFP', 'CBSAFP',
-                                                                                'METDIVFP', 'FUNCSTAT'])
+    assert data_frame_to_filter.crs == county_geom.crs
+    print("CRS Check - Passed")
+    filtered_df = gpd.sjoin(data_frame_to_filter, county_geom, op='within')  # lets add
+    return filtered_df
 
 def filter_datasets_by_parcel_id(parcel_id: int, parcel_df):
     parcel_of_choice = parcel_df[parcel_df["APN"] == int(parcel_id)]
@@ -111,14 +104,23 @@ def tell_me_more_about_my_parcel(parcel_of_choice):
               f"under the ownership of {owner_name}")
 
 if __name__ == "__main__":
+    # check for inputs files and change directory
+    CONDUIT_INPUT_DATA_DIR = 'C:\\Users\\Alfahham\\Basin_Logix\\data_logix\\conduit_input_data' # TODO SPENCER PLEASE CHANGE THIS
+    os.chdir(CONDUIT_INPUT_DATA_DIR)
     check_for_conduit_input_data()
     check_for_conduit_input_files()
+
+    # lets load in all the needed data
     COUNTIES = load_california_counties_data()
-    san_joaquin_geom = get_geom_from_county_name(counties_data_frame=COUNTIES, selected_county="San Joaquin") #nice source of geom for the county but i dont use this somewhere else
+    CROPS = load_california_crops_data()
+    PARCELS = load_california_parcel_data()
+    WATER_DISTRICTS = load_water_districts_data()
 
-
-    single_parcel_df = filter_datasets_by_parcel_id(parcel_id=17328002, parcel_df=load_california_parcel_data())
-    parcel_of_choice = populate_an_aware_parcel(single_parcel_df, water_districts_df=load_water_districts_data())
+    # using some the functions
+    SAN_JOAQUIN_GEOM = get_geom_from_county_name(counties_data_frame=COUNTIES, selected_county="San Joaquin") #nice source of geom for the county but i dont use this somewhere else
+    san_joaquin_crop = filter_data_by_county(data_frame_to_filter=CROPS, county_geom=SAN_JOAQUIN_GEOM)
+    single_parcel_df = filter_datasets_by_parcel_id(parcel_id=17328001, parcel_df=PARCELS)
+    parcel_of_choice = populate_an_aware_parcel(single_parcel_df, water_districts_df=WATER_DISTRICTS)
     parcel_of_choice_df = pd.DataFrame(parcel_of_choice)
     tell_me_more_about_my_parcel(parcel_of_choice_df)
 
